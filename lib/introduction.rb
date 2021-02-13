cb3a3a0a7cfaab489d19b37bf176e6c4bb85ef69
Serkan Sahin <ssahin55@outlook.com>
2021-02-13 22:15:11 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,6 +3,44 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 
+# Define a method called `introduction` that takes in the parameter `name` and outputs
+# the phrase: `"Hi, my name is #{name}."`
+
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+
+# introduction(serkan)
+
+
+
+
+
+
+# **************************************************************************
+
+# Define a method called `introduction_with_language` that takes in two parameters,
+# `name` and `language` and outputs the phrase: `"Hi, my name is #{name} and I am
+# learning to program in #{language}."`
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+
+
+
+
+# ***********************************************************************************************************
+
+# Copy the previous method and name it `introduction_with_language_optional`. It
+# takes in two parameters, `name` and `language`, but the second parameter is
+# optional. The default value for `language` will be "Ruby".
+
+def introduction_with_language_optional(name, language=" Error: No language entered")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
@@ -13,23 +51,7 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+introduction_with_language_optional(name, language)
 
 
 
